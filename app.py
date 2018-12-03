0fa69c19c9d1aa6fb53bd9d2fd3bca153c47d708
soohmatthew <matthewsoh18@gmail.com>
2018-12-04 03:59:06 +0800

    Updated API

--- app.py
+++ app.py
@@ -16,8 +16,11 @@ def user_detail(income, citizen, expenditure, first_choice, second_choice, third
     json_df = df.to_json(orient='split')
     return json_df
 
+import pandas as pd
+
 def apply_filter(input_data):
     df = pd.read_csv("credit_card_scores.csv")
+    
     # filter by eligibility
     if input_data['citizen'] == "Singaporean":
         income_limit = 'income.singapore_conv'
@@ -66,9 +69,32 @@ def apply_filter(input_data):
     df_3_filtered["preferences.debt_servicing"] = preference_dict["preferences.debt_servicing"] * df_2_filtered["interest.rate_conv"]
     df_3_filtered["Final Score"] = df_3_filtered.sum(axis = 1)
     df_3_filtered['card'] = df_2_filtered['card']
+    df_3_filtered['benefits.keyfeatures'] = df_2_filtered['benefits.keyfeatures'].fillna(0)
     df_3_filtered.sort_values(by=["Final Score"], inplace = True, ascending = False)
     df_3_filtered.reset_index(inplace = True)
-    return df_3_filtered.head(3)[['card', 'Final Score']]
+    result = df_3_filtered.head(3)[['card', 'Final Score','benefits.keyfeatures']]
+    
+    cardlinks = {"american" : ['https://www.americanexpress.com/sg/credit-cards/all-cards/'],
+    "bank-of-china" : ['http://www.bankofchina.com/sg/bcservice/bc1/'], # need to do further validation for the card type
+    "maybank" : ['https://apply.maybank.com.sg/cards/#/creditcardstart?sc=acq4'],
+    "cimb" : ['https://www.cimbbank.com.sg/en/personal/products/cards/credit-cards.html'], # need to have .html
+    "posb" : ['https://www.posb.com.sg/personal/cards/credit-cards/posb-everyday-card'],
+    "dbs" : ['https://www.dbs.com.sg/personal/cards/credit-cards/'],
+    "uob" : ['https://www.uob.com.sg/credit-cards/all-cards.html?ms=pweb-cards-home'], #that's it, don't need to do anything else
+    "hsbc" : ['https://www.hsbc.com.sg/1/2/personal/cards/'],
+    "ocbc" : ['https://www.ocbc.com/personal-banking/cards/'], # may need to either !) strip the -, 2) associate with the payment merchant, 3) or do both
+    "ocbc-frank" : ['http://www.frankbyocbc.com/products/cards/credit-card/'],
+    "citi" : ['https://www.citibank.com.sg/gcb/credit_cards/credit-card.htm'],
+    "standard" : ['https://www.sc.com/sg/credit-cards/']} # need to change the name accordingly
+        
+    def get_link(card):
+        for i in cardlinks.keys():
+            if i in card:
+                return cardlinks[i][0]
+        
+    result['card-link'] = result['card'].apply(get_link)
+
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
